01bed6d84f9ac9c6717ba0d352cdece007de7bb5
Jehyun <congmul@hotmail.com>
2022-04-29 15:37:03 -0700

    Add multer routes

--- routes/api/multer-route.js
+++ routes/api/multer-route.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
+const multer = require('multer');
+const upload = multer({dest: 'public/uploads/'});
 
+router.post('/', upload.single('profile'), (req, res) => {
+    try{
+        console.log(req.file);
+        console.log(req.body);
+        res.send(req.file);
+    }catch(err){
+        res.send(400);
+    }
+})
 
 module.exports = router;
