b5a06862ab2effa03f07ceb764e52fb65a464f3a
Jehyun <congmul@hotmail.com>
2022-04-29 16:16:56 -0700

    Updated path

--- routes/api/multer-route.js
+++ routes/api/multer-route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/uploads')
+      cb(null, path.resolve(__dirname, 'build'))
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
